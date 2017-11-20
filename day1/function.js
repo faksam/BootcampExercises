da797fc798e39e665ecc79693eb63a9ad29f4bbe
faksam <fakunlesamuel@gmail.com>
2017-11-20 21:07:31 +0100

    add test for function

--- day1/function.js
+++ day1/function.js
@@ -1,9 +1,38 @@
 module.exports = {
-  words: function(input){
-    var wordOccur = {};
-    input.split(" ").forEach(function (el, i, arr) {
-        wordOccur[el] = wordOccur[el] ? ++wordOccur[el] : 1;
-    });
-    return wordOccur;
-  }
+    words: function (input) {
+    if (typeof input === 'string') {
+        input.trim();
+        input = input.trim().replace(/\s+/g, ' ');
+        var wordOccur = {};
+        input.split(" ").forEach(function (el, i, arr) {
+            wordOccur[el] = wordOccur[el] ? ++wordOccur[el] : 1;
+        });
+        return wordOccur;
+    }
+    else if (typeof input === 'number')
+    {
+        return 'Invalid number input';
+    }
+    else if (typeof input === 'boolean') {
+        return 'Invalid boolean input';
+    }
+    else if (typeof input === 'undefined') {
+        return 'Invalid undefined input';
+    }
+    else if (Array.isArray(input)) {
+        return 'Invalid array input';
+    }
+    else if (typeof input === 'object') {
+        return 'Invalid object input';
+    }
+    else if (typeof input === 'symbol') {
+        return 'Invalid symbol input';
+    }
+    else if (typeof input === 'function') {
+        return 'Invalid function input';
+    }
+    
+    else
+        return 'Invalid input';
+    }
 }
--- day1/test/appTest.js
+++ day1/test/appTest.js
@@ -1,11 +1,54 @@
 const assert = require('chai').assert;
 const app = require('../function');
 
-// Results
-
-describe('words(olly olly in come free)', function () {
-    it('words should return { olly: 2, in: 1, come: 1, free: 1 }', function () {
-        var result = app.words('olly olly in come free');           
+describe("words(olly olly in come free)", () => {
+    //Test to check correct input
+    it('words should trim white spaces', function () {
+        var result = app.words('olly    olly  in come   free');
         assert.deepEqual(result, { olly: 2, in: 1, come: 1, free: 1 });
+    });
+});
+
+describe("words(olly    olly  in come   free)", () => {
+    //Test to check incorrect input with white spaces
+    it('words should return a invalid boolean input', () => {
+        const result = app.words(true)
+        assert.deepEqual(result, 'Invalid boolean input');
+    });
+});
+
+describe("words(true)", () => {
+    //Test to check if input is Boolean
+    it('words should return a invalid boolean input', () => {
+        const result = app.words(true)
+        assert.deepEqual(result, 'Invalid boolean input');
+    });
+});
+
+describe("words(5)", () => {
+    //Test to check if input is number
+    it('words should return invalid number input', () => {
+        assert.equal(app.words(5), "Invalid number input");
+    });
+});
+
+describe("words({ a: 1 })", () => {
+    //Test to check if input is an object
+    it('words({ a: 1 }) should return invalid object input', () => {
+        assert.equal(app.words({ a: 1 }), "Invalid object input");
+    });
+});
+
+describe("words(function () { })", () => {
+    //Test to check if input is a function
+    it('words(function () { }) should return invalid function input', () => {
+        assert.equal(app.words(function () { }), "Invalid function input");
+    });
+});
+
+describe('words(["olly", "olly", "in ", "come", "true"])', () => {
+    //Test to check if input is an array
+    it('words(["olly", "olly", "in", "come", "true"]) should return invalid array input', () => {
+        assert.equal(app.words(["olly", "olly", "in", "come", "true"]), "Invalid array input");
     });
 });
