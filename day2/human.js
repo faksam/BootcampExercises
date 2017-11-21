d126b550b7050026cefff68b61ca119da5b06c93
faksam <fakunlesamuel@gmail.com>
2017-11-21 20:05:27 +0100

    change console.log to return

--- day2/human.js
+++ day2/human.js
@@ -9,28 +9,26 @@ class Human {
     }
 
     toString() {
-        //`${this.name} | ${this.protein}g P :: ${this.carbs}g C :: ${this.fat}g F`
-        return `${this.name} | Age ${this.age} :: Gender ${this.gender}`
+        return `Name: ${this.name} | Age: ${this.age} | Gender: ${this.gender}`
     }
     move() {
-        console.log("I am moving!");
+        return "I am moving!";
     }
     print() {
-        console.log(this.toString());
+        return this.toString();
     }
 }
 class Adult extends Human {
 
     move() {
-        console.log(this.name + " is walking!");
+        return this.name + " is walking!";
     }
     constructor(name, age, gender, occupation) {
         super(name, age, gender);
         this.occupation = occupation;
     }
     print() {
-        super.print();
-        console.log(`${this.name} is a ${this.occupation}`);
+        return `Name: ${this.name} | Age: ${this.age} | Gender: ${this.gender}, ${this.name}  is a ${this.occupation}`;
     }
 
 }
@@ -41,11 +39,10 @@ class Child extends Human {
         this._class = _class;
     }
     move() {
-        console.log(this.name + " is walking!");
+        return this.name + " is walking!";
     }
     print() {
-        super.print();
-        console.log(`${this.name} is in ${this._class}`);
+        return `Name: ${this.name} | Age: ${this.age} | Gender: ${this.gender}, ${this.name}  is in ${this._class}`;
     }
 
 }
@@ -56,11 +53,10 @@ class Baby extends Human {
     }
     move() {
 
-        console.log(this.name + " is crawling!");
+        return this.name + " is crawling!";
     }
     print() {
-        super.print();
-        console.log(`${this.name} is a baby`);
+        return `Name: ${this.name} | Age: ${this.age} | Gender: ${this.gender}, ${this.name}  is a baby`;
     }
 
 }
@@ -70,13 +66,13 @@ const adult = new Adult('Adetunji', 26, "Male", "Farmer");
 const child = new Child('Sola', 6, "female", "primary 2");
 const baby = new Baby('Tola', 1, "female");
 
-human.print(); //
-human.move(); // 
-adult.print(); // 
-adult.move(); // 
-child.print(); // 
-child.move(); // 
-baby.print(); // 
-baby.move(); // 
+human.print();
+human.move();
+adult.print();
+adult.move();
+child.print();
+child.move();
+baby.print();
+baby.move();
 
 
